d21d441b48a2db14bc0dd8653e3fc7f9b7c01a76
JokeGp <jguerrero24@ucol.mx>
2024-02-26 13:00:32 -0600

    feat(css). completed summary component design

---
+++ main.js
@@ -0,0 +1,37 @@
+const entero= 10;
+const flotante_o_decimal=123.9999;
+const booleano = true;
+const char = "a";
+const string = "Violent crimes, well thats in U.S.A, digo Hola Mundo!";
+const array = [1,2,3,4,5,6,7,8];
+const objeto = {
+    nombre: "Pessi",
+    nacionalidad: "Argentina",
+    edad: 36
+};
+const list = ["someday","life its simple in the moonlight", "heart in a cage", "Welcome to japan", "ode to the meets"]
+
+console.log(`tipo de dato entero`)
+console.log(entero);
+console.log(` `)
+console.log(`tipo de dato flotante`);
+console.log(flotante_o_decimal);
+console.log(` `)
+console.log(`tipo de dato booleano`);
+console.log(booleano);
+console.log(` `)
+console.log(`tipo de dato char`)
+console.log(char);
+console.log(` `)
+console.log(`tipo de dato string`);
+console.log(string);
+console.log(` `)
+console.log(`tipo de dato array`);
+console.log(array);
+console.log(` `)
+console.log(`tipo de dato objeto`);
+console.log(objeto);
+console.log(` `)
+console.log(`tipo de dato lista`);
+console.log(list);
+
